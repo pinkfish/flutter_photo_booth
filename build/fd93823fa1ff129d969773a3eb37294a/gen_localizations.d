d7d12b8985be561350a07f3ac52d18d27dd11868
pinkfish <pinkfish@users.noreply.github.com>
2024-01-18 01:02:18 -0800

    Remove build files

--- build/fd93823fa1ff129d969773a3eb37294a/gen_localizations.d
+++
@@ -1,1 +0,0 @@
- C:\\Users\\david\\Documents\\flutter\\froggybooth\\.dart_tool\\flutter_gen\\gen_l10n\\app_localizations_en.dart C:\\Users\\david\\Documents\\flutter\\froggybooth\\.dart_tool\\flutter_gen\\gen_l10n\\app_localizations.dart:  C:\\Users\\david\\Documents\\flutter\\froggybooth\\l10n.yaml C:\\Users\\david\\Documents\\flutter\\froggybooth\\lib/src/localization\\app_en.arb
